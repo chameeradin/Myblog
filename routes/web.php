c963f076c5f1921709e09a03ae00568187d6db34
chameeradin <chameeradinesh12@gmail.com>
2021-01-13 21:17:04 +0530

    create post controller, model, migration files and blades

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
@@ -32,6 +33,7 @@ Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
 
 
-Route::get('/posts', function () {
-    return view('posts.index');
-})->name('posts')->middleware('auth');
+Route::get('/posts', [PostController::class, 'index'])->name('post');
+Route::post('/posts', [PostController::class, 'store']);
+
+
